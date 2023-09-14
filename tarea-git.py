66e07bc3048e8098878a432767ed67b44433e120
unknown <fran.millan95@gmail.com>
2023-09-14 17:20:17 -0300

    Agregando la tarea al staging area

---
+++ tarea-git.py
@@ -0,0 +1,17 @@
+class Calculadora:
+
+    def sumar(a, b) -> int:
+        """Retorna la suma de dos numeros enteros"""
+        return a+b
+
+    def restar(a, b) -> int:
+        """Retorna la resta de dos numeros enteros"""
+        return a-b
+
+    def multiplicar(a, b) -> int:
+        """Retorna la multiplicacion de dos numeros enteros"""
+        return a*b
+
+    def dividir(a, b) -> float:
+        """Retorna la division de dos numeros de punto flotante"""
+        return a/b
